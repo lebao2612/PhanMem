75fd1ffaaa83d57a50b5c2e7ba833915966c5ce5
lebao2612 <btaldforever@gmail.com>
2025-05-20 11:33:27 +0700

    setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/HomePage"
+import "@fontsource/montserrat";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className='text-black text-5xl'>
-      Hello World
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   )
 }
 
