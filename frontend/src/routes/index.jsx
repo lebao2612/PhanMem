7c3e7f873d2cdc866fd59ea1216c24eb491b7ec1
Hồ Quang Sang <sangdinhban@gmail.com>
2025-06-30 07:36:49 +0700

    fix(fe): fix invalid or missing module import

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -3,12 +3,12 @@ import Login from "../pages/LoginPage";
 import Dashboard from "../pages/DashBoardPage";
 import EditVideo from "../pages/EditVideoPage";
 
-import TestReview from "../pages/TestReviewPage";
+// import TestReview from "../pages/TestReviewPage";
 import GoogleOAuthCallbackPage from "../pages/GoogleOAuthCallbackPage";
 
 const publicRoutes = [
   { path: "/login", component: Login },
-  { path: "/review", component: TestReview },
+  // { path: "/review", component: TestReview },
   { path: "/login/google/oauth/callback", component: GoogleOAuthCallbackPage },
 ];
 const privateRoutes = [
